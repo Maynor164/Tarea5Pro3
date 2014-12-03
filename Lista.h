11ae1c6985eb0211e94477e7337e06e1c3527e40
Alumno <alumno@localhost.localdomain>
2014-12-03 08:31:45 -0600

    primer commit

---
+++ Lista.h
@@ -0,0 +1,135 @@
+#ifndef LISTA_H
+#define LISTA_H
+
+#include <iostream>
+#include "Nodo.h"
+using namespace std;
+
+//Trabajo en grupo realizado con
+//Kevin Lanza
+//Iveth Sabillon
+//Merary Fernandez
+template <typename Tipo>
+class Lista
+{
+public:
+    Nodo<Tipo>* inicio;
+
+    Lista()
+    {
+        inicio = NULL;
+    }
+
+    void imprimir()
+    {
+        for(Nodo<Tipo>* temp = inicio;
+            temp!=NULL;
+            temp = temp->sig)
+        {
+            cout<<temp->num<<endl;
+        }
+    }
+
+    void agregarAlInicio(Tipo valor_nuevo)
+    {
+        Nodo<Tipo> *nuevo = new Nodo<Tipo>(valor_nuevo);
+
+        if(inicio == NULL)
+        {
+            inicio = nuevo;
+        }else
+        {
+            nuevo->sig = inicio;
+            inicio = nuevo;
+        }
+    }
+
+    void agregarAlFinal(Tipo valor_nuevo)
+    {
+        Nodo<Tipo> *nuevo = new Nodo<Tipo>(valor_nuevo);
+
+        if(inicio == NULL)
+        {
+            inicio = nuevo;
+        }else
+        {
+            Nodo<Tipo>*temp = inicio;
+            while(temp->sig!=NULL)
+            {
+                temp=temp->sig;
+            }
+            temp->sig = nuevo;
+        }
+    }
+
+    void borrar(Tipo valor_a_borrar)
+    {
+        if(inicio->num == valor_a_borrar)
+        {
+            Nodo<Tipo>* borrar = inicio;
+            inicio = inicio->sig;
+            delete borrar;
+        }else
+        {
+            Nodo<Tipo>* temp = inicio;
+            while(temp->sig->num!=valor_a_borrar)
+            {
+                temp=temp->sig;
+                if(temp->sig==NULL)
+                    return;
+            }
+            Nodo<Tipo>* borrar = temp->sig;
+            temp->sig = temp->sig->sig;
+            delete temp->sig;
+        }
+    }
+
+    //Devuelve true si la lista esta vacia, de lo contrario devuelve false
+    bool estaVacia()
+//Tomando referencia de http://blog.alguien.at/2010/12/lista-enlazada-simple-en-cc.html
+//Linea de codigo 77
+{
+//Creamos un ciclo para evaluar si el nuestra variable de inicio esta vacia si lo esta muestra true de lo contrario false
+     if(inicio == NULL)
+        {
+            return true;
+        }
+     return false;
+}
+
+    //Devuelve el valor almacenado en el primer nodo de la lista
+    Tipo obtenerPrimerValor()
+    {
+//Regresamos el primer valor de nuestro nodo
+    return inicio->valor;
+    }
+
+    //Devuelve el valor almacenado en el ultimo nodo de la lista
+    Tipo obtenerUltimoValor()
+{
+   Nodo<Tipo>*temp = inicio;
+//Usamos el ciclo para recorrer la lista
+    while(temp->sig!=NULL)
+    {
+        temp= temp->sig;
+    }
+    return temp->valor;
+
+}
+    //Devuelve el valor mayor almacenado en la lista
+    Tipo obtenerMayor()
+ {
+    Nodo<Tipo>*temp = inicio;
+    int mayor = -9999;
+    while(temp!=NULL)
+        {
+            if(mayor<temp->valor)
+            {
+                mayor = temp->valor;
+            }
+            temp=temp->sig;
+        }
+        return mayor;
+ }
+};
+#endif // LISTA_H
